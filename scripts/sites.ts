18337bbd64f3d1ef9d470e61121402a8e2d9bb34
SessionHu <102411014+SessionHu@users.noreply.github.com>
2024-04-03 22:11:39 +0800

    refactor: json加载

---
+++ scripts/sites.ts
@@ -0,0 +1,60 @@
+interface SiteGroup {
+    id: string,
+    name: string,
+    links: Sitebox[]
+}
+
+interface Sitebox {
+    id: string,
+    title: string,
+    titlecn: string,
+    url: string,
+    icon: string,
+    desc: string,
+}
+
+async function fillGroupInfo(): Promise<void> {
+    const ctt: HTMLDivElement = document.querySelector("div.content") as HTMLDivElement;
+    const gpres: SiteGroup[] = await (await fetch("/assets/sites.json")).json();
+    for(const gp of gpres){
+        // div.gp
+        const gpe: HTMLDivElement = document.createElement("div");
+        gpe.className = "gp";
+        ctt.insertAdjacentElement("beforeend",gpe);
+        // div.gptitle & hr
+        gpe.insertAdjacentHTML("beforeend",`
+            <div class="gptitle" id="${gp.id}">
+                <h2>${gp.name}</h2>
+            </div>
+            <hr />
+        `);
+        // div.gpframe
+        const gpframe: HTMLDivElement = document.createElement("div");
+        gpframe.className = "gpframe";
+        for(const sitebox of gp.links) {
+            gpframe.insertAdjacentHTML("beforeend",`
+                <!--${sitebox.title}-->
+                <div class="sitebox" id="${sitebox.id}">
+                    <a class="siteboxlink" title="${sitebox.title}" href="${sitebox.url}">
+                        <div class="siteboxframe">
+                            <div class="siteboxheader">
+                                <div class="siteicon">
+                                    <img alt=" " src="${sitebox.icon}" />
+                                </div>
+                                <div class="sitetitle">
+                                    <span>${sitebox.titlecn}</span>
+                                </div>
+                            </div>
+                            <div class="sitedescription">
+                                <span>${sitebox.desc}</span>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            `);
+        }
+        gpe.insertAdjacentElement("beforeend",gpframe);
+    }
+}
+
+window.addEventListener<"load">("load",()=>fillGroupInfo());
