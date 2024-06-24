5c13e9024001a9db5de0c15faac19c7234c7c279
SessionHu <102411014+SessionHu@users.noreply.github.com>
2024-06-24 14:02:17 +0800

    feat: round search form & removable common siteboxes

--- scripts/sites.ts
+++ scripts/sites.ts
@@ -20,24 +20,66 @@ async function fillGroupInfo(): Promise<void> {
         // div.gp
         const gpe: HTMLDivElement = document.createElement("div");
         gpe.className = "gp";
+        gpe.id = gp.id;
         // div.gptitle & hr
         gpe.insertAdjacentHTML("beforeend",`
-            <h2 class="gptitle" id="${gp.id}">${gp.name}</h2>
+            <h2 class="gptitle">${gp.name}</h2>
         `);
         // div.gpframe
         const gpframe: HTMLDivElement = document.createElement("div");
         gpframe.className = "gpframe";
         for(const sitebox of gp.links) {
-            gpframe.insertAdjacentHTML("beforeend",`
-                <!--${sitebox.title}-->
-                <a class="siteboxlink" id="${sitebox.id}" title="${sitebox.title}" href="${sitebox.url}">
-                    <span class="sitetitle" style="background-image:url('${sitebox.icon}');">${sitebox.titlecn}</span>
-                    <span class="sitedescription">${sitebox.desc}</span>
-                </a>
-            `);
+            // a.siteboxlink
+            const a: HTMLAnchorElement = document.createElement("a");
+            a.className = "siteboxlink";
+            a.id = sitebox.id;
+            a.title = sitebox.title;
+            a.href = sitebox.url;
+            gpframe.appendChild(a);
+            // span.sitetitle
+            const sitetitle: HTMLSpanElement = document.createElement("span");
+            sitetitle.className = "sitetitle";
+            sitetitle.style.backgroundImage = `url('${sitebox.icon}')`;
+            sitetitle.innerText = sitebox.titlecn;
+            a.appendChild(sitetitle);
+            // span.sitedescription
+            if(gp.id !== "common") {
+                const sitedesc = new HTMLSpanElement();
+                sitedesc.className = "sitedescription";
+                sitedesc.innerText = sitebox.desc;
+                a.appendChild(sitedesc);
+            } else a.addEventListener("contextmenu", (ev: MouseEvent) => {
+                // only one cross
+                if(sitetitle.childElementCount < 1) {
+                    ev.preventDefault();
+                    // create element
+                    const cross: HTMLButtonElement = document.createElement("button");
+                    cross.className = "cross";
+                    // remove cross and link
+                    const remover = (rev?: MouseEvent) => {
+                        // remove cross and link
+                        rev?.preventDefault();
+                        a.remove();
+                        if(cross.parentElement !== null) cross.remove();
+                        // if no link left
+                        if(gpframe.childElementCount < 1) {
+                            gpe.remove();
+                            fillGroupInfo();
+                        }
+                    };
+                    if(window.innerWidth <= 220) {
+                        remover();
+                    } else {
+                        cross.addEventListener("click", remover);
+                        sitetitle.appendChild(cross);
+                        // auto remove cross 3s later
+                        window.setTimeout(() => cross.remove(), 3e3);
+                    }
+                }
+            });
         }
-        gpe.insertAdjacentElement("beforeend",gpframe);
-        ctt.insertAdjacentElement("beforeend",gpe);
+        gpe.appendChild(gpframe);
+        ctt.appendChild(gpe);
     }
 }
 
